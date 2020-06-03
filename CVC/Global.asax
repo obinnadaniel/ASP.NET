81a9e22d2272b0f5baab84eeb3822cd5d5e5761e
Obinna Daniel <obinnadaniel30@yahoo.com>
2020-06-03 18:34:21 +0100

    initial commit

---
+++ CVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CVC.MvcApplication" Language="C#" %>
---
+++ CVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CVC.Startup))]
+namespace CVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
